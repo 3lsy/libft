da2b12caf3c46e211434b40bfbc0093acb68ca58
3lsy <>
2022-11-01 18:39:07 +0100

    [+]ft_isalpha, ft_isdigit, ft_isalnum, ft_isascii, ft_isprint

--- libft.h
+++ libft.h
@@ -6,13 +6,17 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 18:32:07 by echavez-          #+#    #+#             */
-/*   Updated: 2022/10/28 18:38:57 by echavez-         ###   ########.fr       */
+/*   Updated: 2022/11/01 18:35:12 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_isalpha(char c);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
 
 #endif
