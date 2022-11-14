9c8b03f5ccf237e51ed3914e8f52f59136704dea
echavez- <echavez-@student.42.fr>
2022-11-14 17:35:11 +0100

    [+]ft_strlcpy.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 18:32:07 by echavez-          #+#    #+#             */
-/*   Updated: 2022/11/11 21:02:48 by echavez-         ###   ########.fr       */
+/*   Updated: 2022/11/12 21:06:27 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n);
 void 	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
 #endif
