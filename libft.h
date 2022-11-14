b7ab1d815246d360a25cd1116b7e14a531325eb6
echavez- <echavez-@student.42.fr>
2022-11-14 18:05:14 +0100

    [+]ft_tolower.c && [fix] Condition (ft_toupper.c)

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 18:32:07 by echavez-          #+#    #+#             */
-/*   Updated: 2022/11/14 17:45:28 by echavez-         ###   ########.fr       */
+/*   Updated: 2022/11/14 18:03:20 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
