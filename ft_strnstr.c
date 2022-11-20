4f753656555a3e3e98fe632694c69233d73688cf
echavez- <echavez-@student.42.fr>
2022-11-20 21:06:22 +0100

    [+]strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 18:28:33 by echavez-          #+#    #+#             */
+/*   Updated: 2022/11/17 22:55:27 by echavez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char * s1, const char * s2, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!s2[0])
+		return ((char *)s1 + ft_strlen(s1));
+	i = 0;
+	while (s1[i] && i < len)
+	{
+		j = 0;
+		while (i + j < len && s2[j] && s1[i + j] && s1[i + j] == s2[j])
+			j++;
+		if (s2[j] == '\0')
+			return ((char *)s1 + i);
+		i++;
+	}
+	return (NULL);
+}
