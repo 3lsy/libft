fe642805458e89f50c17d3baa2b84d3027b0d21c
echavez- <echavez-@student.42.fr>
2022-11-20 21:05:52 +0100

    [+]strdup

---
+++ ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/20 17:42:45 by echavez-          #+#    #+#             */
+/*   Updated: 2022/11/20 18:07:18 by echavez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *src)
+{
+	int		i;
+	int		lens;
+	char	*cpy;
+
+	lens = 0;
+	while (src[lens])
+		lens++;
+	cpy = malloc(sizeof(char) * (lens + 1));
+	if (!cpy)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		cpy[i] = src[i];
+		i++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
+}
