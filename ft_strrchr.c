83c3dbe12b250c607a76ebe59b8762f5201de8cf
echavez- <echavez-@student.42.fr>
2022-11-20 21:03:31 +0100

    [fix]condition

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 18:40:15 by echavez-          #+#    #+#             */
-/*   Updated: 2022/11/14 18:53:54 by echavez-         ###   ########.fr       */
+/*   Updated: 2022/11/16 20:55:31 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,18 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	size_t	s_len;
-	size_t	i;
+	int		i;
 
+	c = (char)c;
 	s_len = ft_strlen(s);
 	i = s_len - 1;
-	while (i < 0)
+	while (i >= 0)
 	{
 		if (s[i] == c)
 			return ((char *)(&s[i]));
 		i--;
 	}
 	if (c == '\0')
-		return ((char *)(&s[s_len - 1]));
+		return ((char *)(&s[s_len]));
 	return (NULL);
 }
