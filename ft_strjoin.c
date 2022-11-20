3c5f1b99f3e5b1a9f4b174be4c151e0e6271b717
echavez- <echavez-@student.42.fr>
2022-11-20 21:06:08 +0100

    [+]strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/20 19:22:35 by echavez-          #+#    #+#             */
+/*   Updated: 2022/11/20 19:49:01 by echavez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	i;
+	size_t	j;
+	char	*s;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	s = malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (s1_len > i)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2_len > j)
+		s[i++] = s2[j++];
+	s[i] = 0;
+	return (s);
+}
