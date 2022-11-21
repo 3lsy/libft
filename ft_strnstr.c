618ebb9f67393d0d1519fbaa94d9d8c09f6647ba
echavez- <echavez-@student.42.fr>
2022-11-21 09:52:53 +0100

    [fix]norme (ft_strnstr)

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,13 +6,13 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 18:28:33 by echavez-          #+#    #+#             */
-/*   Updated: 2022/11/17 22:55:27 by echavez-         ###   ########.fr       */
+/*   Updated: 2022/11/21 09:52:12 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char * s1, const char * s2, size_t len)
+char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 {
 	size_t	i;
 	size_t	j;
