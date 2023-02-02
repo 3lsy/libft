c37d60c22ff177c9a5d2d3089e6f9a9566f1d825
echavez- <echavez-@student.42.fr>
2023-02-02 16:17:16 +0100

    [+]ft_uputnbr_base

---
+++ ft_uputnbr_base.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_uputnbr_base.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/06 01:48:51 by echavez-          #+#    #+#             */
+/*   Updated: 2022/12/06 11:51:56 by echavez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+uintmax_t	ft_uputnbr_base(uintmax_t nbr, char *base)
+{
+	uintmax_t		i;
+	uintmax_t		size;
+	char			res[42];
+	uintmax_t		written;
+
+	if (nbr == 0)
+		return (write(1, "0", 1));
+	size = ft_strlen(base);
+	i = 0;
+	while (nbr)
+	{
+		res[i++] = base[nbr % size];
+		nbr = nbr / size;
+	}
+	written = i;
+	while (i != 0)
+		write(1, &res[--i], 1);
+	return (written);
+}
