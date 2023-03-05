75060d43a633d16ae418e0212ffe7a308161a69d
3lsy <echavez-@student.42.fr>
2023-03-05 15:04:47 +0100

    [+]ft_insert_first.c

--- ft_insert_first.c
+++ ft_insert_first.c
@@ -1,10 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_insert_first.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/05 14:54:41 by echavez-          #+#    #+#             */
+/*   Updated: 2023/03/05 15:02:50 by echavez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void	ft_insert_first(void *content, t_size size, t_doubly **start, t_doubly *end)
+void	ft_insert_first(void *content, size_t size, t_doubly **start, t_doubly *end)
 {
 	t_doubly	*new;
 
 	new = ft_create_node(content, size);
+	if (!new)
+		return ;
 	if (!(*start))
 	{
 		new->next = new;
--- libft.h
+++ libft.h
@@ -6,10 +6,10 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 18:32:07 by echavez-          #+#    #+#             */
-/*   Updated: 2023/03/02 16:51:47 by marvin           ###   ########.fr       */
+/*   Updated: 2023/03/05 15:02:22 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-s
+
 #ifndef LIBFT_H
 # define LIBFT_H
 
@@ -87,5 +87,6 @@ int			ft_puterror(char *start, char *cmd, char *end);
 t_list		*ft_listnew(void *content, size_t size);
 void		ft_listdel(t_list **head);
 t_doubly	*ft_create_node(void *content, size_t size);
+void		ft_insert_first(void *content, size_t size, t_doubly **start, t_doubly *end);
 
 #endif
