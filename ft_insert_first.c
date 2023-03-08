58e1cd1811437bcef59e0457733846d6ee2fc204
3lsy <echavez-@student.42.fr>
2023-03-08 19:59:34 +0100

    [fix]start null segfault

--- ft_insert_first.c
+++ ft_insert_first.c
@@ -6,7 +6,7 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 14:54:41 by echavez-          #+#    #+#             */
-/*   Updated: 2023/03/05 16:30:57 by echavez-         ###   ########.fr       */
+/*   Updated: 2023/03/08 19:34:42 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,6 @@
 
 void	ft_insert_first(t_doubly *new, t_doubly **start)
 {
-	t_doubly	*end;
-
-	if (!new)
-		return ;
-	end = (*start)->prev;
-	if (!(*start))
-	{
-		new->next = new;
-		new->prev = new;
-		*start = new;
-		return ;
-	}
-	end->next = new;
-	(*start)->prev = new;
-	new->prev = end;
-	new->next = *start;
+	ft_insert_last(new, start);
 	*start = new;
 }
