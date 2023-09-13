77f4d6f1c86dac7cd3d33abfb45846502480254a
AndreIglesias <ciglesia@student.42.fr>
2023-09-13 17:34:50 +0200

    strcpy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 18:32:07 by echavez-          #+#    #+#             */
-/*   Updated: 2023/09/12 15:29:57 by echavez-         ###   ########.fr       */
+/*   Updated: 2023/09/13 17:33:22 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,7 @@ void			ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
 void			*ft_memmove(void *dest, const void *src, size_t n);
 size_t			ft_strlcpy(char *dst, const char *src, size_t size);
+size_t			ft_strcpy(char *dst, const char *src);
 size_t			ft_strlcat(char *dst, const char *src, size_t size);
 int				ft_toupper(int c);
 int				ft_tolower(int c);
